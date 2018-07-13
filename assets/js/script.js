a554317171a2137932e430ae9fe335feee5ace73
Scott McDowell <mcdowell0202@gmail.com>
2018-07-13 16:18:12 -0600

    071318

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,18 @@
 // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyAtMuN752qE1mb4QHbDthQuZlF6DyM3KyM",
-    authDomain: "train-scheduler-8b15e.firebaseapp.com",
-    databaseURL: "https://train-scheduler-8b15e.firebaseio.com",
-    projectId: "train-scheduler-8b15e",
-    storageBucket: "",
-    messagingSenderId: "752448060367"
-  };
-  firebase.initializeApp(config);
+var config = {
+  apiKey: "AIzaSyAtMuN752qE1mb4QHbDthQuZlF6DyM3KyM",
+  authDomain: "train-scheduler-8b15e.firebaseapp.com",
+  databaseURL: "https://train-scheduler-8b15e.firebaseio.com",
+  projectId: "train-scheduler-8b15e",
+  storageBucket: "train-scheduler-8b15e.appspot.com",
+  messagingSenderId: "752448060367"
+};
+firebase.initializeApp(config);
+// database initialization
+let database = firebase.database();
 // document ready function
 $(document).ready(function() {
-// database initialization
-  let database = firebase.database();
+
 // intake data click function
     $("#submit").on("click", function(event) {
 
@@ -22,8 +23,6 @@ $(document).ready(function() {
       destination: $("#destination").val().trim(),
       trainTime: $("#train-time").val().trim(),
       frequency: $("#frequency").val(),
-      minutesAway: 0,
-      nextArrival: "2:00",
       dateAdded: firebase.database.ServerValue.TIMESTAMP
     }
 // pushing train info object to database
@@ -40,25 +39,16 @@ $(document).ready(function() {
 
   let name = snapshot.val().trainName;
   let dest = snapshot.val().destination;
-  let time = snapshot.val().nextArrival;
-  let freq = snapshot.val().frequency;
-  let minAway = snapshot.val().minutesAway;
-  let startTime = snapshot.val().trainTime;
+  let frequencyVar = snapshot.val().frequency;
+  let firstTrain = snapshot.val().trainTime;
 // math to calculate next train arrival
-  let frequencyVar = freq;
-  let firstTrain = startTime;
-  let firstTimeConverted = moment(firstTrain, "HH:mm").subtract(1, "years");
-  let currentTime = moment().format("LT");
-  let diffTime = moment().diff(moment(firstTrain), "minutes");
+  let firstTimeConverted = moment(firstTrain, "hh:mm").subtract(1, "years");
+  let diffTime = moment().diff(moment(firstTimeConverted), "minutes");
   let remainder = diffTime % frequencyVar;
   let minutesTillTrain = frequencyVar - remainder;
-  let nextTrain = moment().add(minutesTillTrain, "minutes");
-
-  minAway = minutesTillTrain;
-
-  time = moment(nextTrain).format("hh:mm a");
-
-  let newRow = $("<tr><td>" + name + "</td><td>" + dest + "</td><td>" + freq + "</td><td>" + time + "</td><td>" + minAway + "</td></tr>");
+  let nextTrain = moment().add(minutesTillTrain, "m").format("LT");
+  console.log(minutesTillTrain);
+  let newRow = $("<tr><td>" + name + "</td><td>" + dest + "</td><td>" + frequencyVar + "</td><td>" + nextTrain + "</td><td>" + minutesTillTrain + "</td></tr>");
 
   $("#table-body").append(newRow);
 
